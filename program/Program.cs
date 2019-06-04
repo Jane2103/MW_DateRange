c83d3276338d71a2a3d60df72628687c5d648f0e
Jan <jan.lakomy.programista@gmail.com>
2019-06-04 11:07:46 +0200

    Refactoring 2

--- program/Program.cs
+++ program/Program.cs
@@ -1,28 +1,21 @@
 ﻿using System;
 
-namespace program
+namespace DateRange
 {
     public class Program
     {
-        const int FirstDayIndex = 0;
-        const int FirstMonthIndex = 3;
-        const int InsertSubstringDayIndex = 5;
-        const int FirstRemoveIndex = 2;
-        const int SecondRemoveIndex = 5;
-        const string FormatString = "d";
+        const string FormatString = "dd.MM.yyyy";
         const string ExceptionMessage = "Invalid input format.Input shall be provided: dd.mm.yyyy dd.mm.yyyy. " +
                     "First input shall indicate earlier date than the second one. Example: 21.03.2019 31.05.2019";
 
         public static void Main(string[] args)
         {
-            //var startDateAsString = args[0];
-            //var endDateAsString = args[1];
-            var startDateAsString = "29.02.2019";
-            var endDateAsString = "21.03.2019";
+            var startDateAsString = args[0];
+            var endDateAsString = args[1];            
 
             try
             {
-                printRange(startDateAsString, endDateAsString);         
+                PrintRange(startDateAsString, endDateAsString);         
             }
             catch (FormatException)
             {
@@ -30,82 +23,30 @@ namespace program
             }
         }
 
-        public static void printRange(string startDateAsString, string endDateAsString)
+        public static void PrintRange(string startDateAsString, string endDateAsString)
         {
-            if (!isNotText(startDateAsString) || !isNotText(endDateAsString)) throw new FormatException();
-
-            startDateAsString = convertStringToValidForm(startDateAsString);            
-            endDateAsString = convertStringToValidForm(endDateAsString);            
-
             var startDate = DateTime.ParseExact(startDateAsString, FormatString, System.Globalization.CultureInfo.InvariantCulture);
             var endDate = DateTime.ParseExact(endDateAsString, FormatString, System.Globalization.CultureInfo.InvariantCulture);
                         
             var datesComparisonResult = DateTime.Compare(endDate, startDate);            
             if (datesComparisonResult != 1) throw new FormatException();
 
-            if ((startDate.Year == endDate.Year) && (startDate.Month == endDate.Month))
-            {
-                var firstDayAsString = AddZeroIfNeeded(startDate.Day);
-                var secondDayAsString = AddZeroIfNeeded(endDate.Day);
-                var monthAsString = AddZeroIfNeeded(startDate.Month);
+            var firstDayAsString = AddZeroIfNeeded(startDate.Day);
+            var secondDayAsString = AddZeroIfNeeded(endDate.Day);
+            var firstMonthAsString = AddZeroIfNeeded(startDate.Month);
+            var secondMonthAsString = AddZeroIfNeeded(endDate.Month);
 
-                Console.WriteLine($"{firstDayAsString}-{secondDayAsString}.{monthAsString}.{startDate.Year}");
-            }
+            if ((startDate.Year == endDate.Year) && (startDate.Month == endDate.Month))
+                Console.WriteLine($"{firstDayAsString}-{secondDayAsString}.{firstMonthAsString}.{startDate.Year}");                       
 
             if ((startDate.Year == endDate.Year) && (startDate.Month != endDate.Month))
-            {
-                var firstDayAsString = AddZeroIfNeeded(startDate.Day);
-                var secondDayAsString = AddZeroIfNeeded(endDate.Day);
-                var firstMonthAsString = AddZeroIfNeeded(startDate.Month);
-                var secondMonthAsString = AddZeroIfNeeded(endDate.Month);
+                Console.WriteLine($"{firstDayAsString}.{firstMonthAsString}-{secondDayAsString}.{secondMonthAsString}.{startDate.Year}");            
 
-                Console.WriteLine($"{firstDayAsString}.{firstMonthAsString}-{secondDayAsString}.{secondMonthAsString}.{startDate.Year}");
-            }
-
-            if (startDate.Year != endDate.Year)
-            {
-                var firstDayAsString = AddZeroIfNeeded(startDate.Day);
-                var secondDayAsString = AddZeroIfNeeded(endDate.Day);
-                var firstMonthAsString = AddZeroIfNeeded(startDate.Month);
-                var secondMonthAsString = AddZeroIfNeeded(endDate.Month);
-                
+            if (startDate.Year != endDate.Year)                         
                 Console.WriteLine($"{firstDayAsString}.{firstMonthAsString}.{startDate.Year}" +
-                    $"-{secondDayAsString}.{secondMonthAsString}.{endDate.Year}");
-            }
+                    $"-{secondDayAsString}.{secondMonthAsString}.{endDate.Year}");            
         }
 
-        private static bool isNotText(string stringAsDate)
-        {
-            var resolution = true;
-            if (stringAsDate.Length != 10) return false;
-            for (int i = 0; i < stringAsDate.Length; i++)
-            {
-                if ((stringAsDate[i] >= '0' && stringAsDate[i] <= '9') || stringAsDate[i] == '.') resolution &= true;
-                else resolution = false;
-            }
-            return resolution;
-        }
-
-        public static string convertStringToValidForm(string dateAsString)
-        {
-            var substringDay = dateAsString.Substring(FirstDayIndex, 2);
-            var substringMonth = dateAsString.Substring(FirstMonthIndex, 2);
-            var convertedDate = dateAsString.Insert(FirstDayIndex, substringMonth);
-
-            convertedDate = convertedDate.Insert(InsertSubstringDayIndex, substringDay);
-            convertedDate = convertedDate.Remove(FirstRemoveIndex, 2);
-            convertedDate = convertedDate.Remove(SecondRemoveIndex, 2);
-
-            convertedDate = convertedDate.Replace('.', '/');
-
-            return convertedDate;
-        }
-
-        private static string AddZeroIfNeeded(int date)
-        {
-            string numberAsString;
-
-            return date < 10 ? numberAsString = "0" + date : numberAsString = date.ToString();
-        }
+        public static string AddZeroIfNeeded(int date) => date < 10 ? "0" + date : date.ToString();
     }
 }
---
+++ programTest/ProgramTests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DateRange.UnitTests
+{
+    [TestClass]
+    public class ProgramTests
+    {              
+        [DataRow("01.01.2017", "05.01.2017", "01-05.01.2017")]
+        [DataRow("01.01.2017", "05.02.2017", "01.01-05.02.2017")]
+        [DataRow("01.01.2016", "05.01.2017", "01.01.2016-05.01.2017")]
+        [DataRow("21.01.2016", "31.01.2016", "21-31.01.2016")]
+        [DataRow("21.01.2016", "22.02.2016", "21.01-22.02.2016")]
+        [DataRow("24.07.2018", "24.03.2019", "24.07.2018-24.03.2019")]
+        [DataTestMethod]
+        public void PrintRange_WithCorrectInput_ShouldReturnDateRange(string input1, string input2, string expected)
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);                
+
+                DateRange.Program.PrintRange(input1, input2);
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(expected, result);
+            }            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void PrintRange_WithIncorrectDateType_ShouldThrowException()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                var input1 = "29.02.2019";
+                var input2 = "05.04.2019";
+
+                DateRange.Program.PrintRange(input1, input2);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void PrintRange_WithEqualDateValues_ShouldThrowException()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                var input1 = "21.03.2019";
+                var input2 = "21.03.2019";
+
+                DateRange.Program.PrintRange(input1, input2);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void PrintRange_WithStartDateLaterThanEndDate_ShouldThrowException()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                var input1 = "21.03.2019";
+                var input2 = "21.02.2019";          
+
+                DateRange.Program.PrintRange(input1, input2);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void PrintRange_WithInvalidInput_ShouldThrowException()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                var input1 = "32.03.12-3";
+                var input2 = "21.02";
+
+                DateRange.Program.PrintRange(input1, input2);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void PrintRange_WithTextInput_ShouldThrowException()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                var input1 = "making";
+                var input2 = "waves";
+
+                DateRange.Program.PrintRange(input1, input2);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void PrintRange_WithEmptyInput_ShouldThrowException()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                var input1 = "";
+                var input2 = "";
+
+                DateRange.Program.PrintRange(input1, input2);
+            }
+        }
+
+        [DataRow(2, "02")]
+        [DataRow(5, "05")]
+        [DataRow(9, "09")]        
+        [DataTestMethod]
+        public void AddZeroIfNeeded_WithNumberLessThanTen_ShouldAddZeroPrefix(int input, string expected)
+        {
+            var result = DateRange.Program.AddZeroIfNeeded(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [DataRow(20, "20")]
+        [DataRow(15, "15")]
+        [DataRow(10, "10")]
+        [DataTestMethod]
+        public void AddZeroIfNeeded_WithNumberGreaterOrEqualToTen_ShouldParseIntToString(int input, string expected)
+        {
+            var result = DateRange.Program.AddZeroIfNeeded(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+
+    }
+}
